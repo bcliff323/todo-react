79e6c73899c50a7ecaa57c7c67de60a62b679cee
Brian Clifford <bcliff323@gmail.com>
2022-01-22 13:46:01 -0500

    Add new Page

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM, {render} from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ListDetail from './pages/ListDetail';
 import LocalStorageProvider from './components/LocalStorageProvider';
 import useLocalStorage from "use-local-storage";
 
@@ -11,6 +12,11 @@ export default function App() {
         <Route path="/" element={
           <LocalStorageProvider>
             <Home />
+          </LocalStorageProvider>
+        } />
+        <Route path="/list/:id" element={
+          <LocalStorageProvider>
+            <ListDetail />
           </LocalStorageProvider>
         } />
       </Routes>
---
+++ src/pages/ListDetail.tsx
@@ -0,0 +1,100 @@
+import {FormEvent, useState, useContext} from 'react';
+import {DragDropContext, Droppable, Draggable, DropResult} from "react-beautiful-dnd";
+import FadeIn from 'react-fade-in';
+import Layout from '../components/Layout';
+import EditableText from '../components/EditableText';
+import {LocalStorageContext} from '../context/LocalStorageContext';
+import cloneDeep from 'lodash/cloneDeep';
+import {TodoList} from '../types';
+import "../css/styles.css";
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export default function ListDetail(props: Props) {
+	const {
+		savedListData,
+		addNewList,
+		updateListTitle,
+		updateListOrder,
+		deleteList
+	} = useContext(LocalStorageContext);
+
+	const [listName, setListName] = useState<string>("");
+
+
+	function handleSubmit(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		addNewList(listName);
+		setListName("");
+	}
+
+	function onDragEnd(result: DropResult) {
+		if (!result.destination) {
+			return;
+		}
+
+		if (result.destination.index === result.source.index) {
+			return;
+		}
+
+		updateListOrder(parseInt(result.draggableId, 10), result.source.index, result.destination.index);
+	}
+
+	return (
+		<Layout>
+			<h1>List Detail Page</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					name="values"
+					value={listName}
+					placeholder="Enter List Name"
+					onChange={
+						(e) => {
+							setListName(e.target.value || "");
+						}
+					} />
+				<button type="submit">Go</button>
+			</form>
+			<div>
+				<DragDropContext onDragEnd={onDragEnd}>
+					<Droppable droppableId="list">
+						{provided => (
+							<div ref={provided.innerRef} {...provided.droppableProps}>
+								{savedListData && savedListData.map(
+									(list, i) => {
+										const dId = (list as TodoList).ordinal?.toString();
+										return (
+											<Draggable key={(list as TodoList).ordinal} draggableId={dId} index={(list as TodoList).ordinal}>
+												{provided => (
+													<div ref={provided.innerRef}
+														{...provided.draggableProps}
+														{...provided.dragHandleProps}>
+														<FadeIn>
+															<EditableText
+																text={(list as TodoList).title}
+																saveText={(title: string) => {
+																	const i = parseInt((list as TodoList).id, 10);
+																	updateListTitle(title, i);
+																}}
+																deleteList={() => {	
+																	const i = parseInt((list as TodoList).id, 10);
+																	deleteList(i);
+																}} />
+														</FadeIn>
+													</div>
+												)}
+											</Draggable>
+										);
+									}
+								)}
+								{provided.placeholder}
+							</div>
+						)}
+					</Droppable>
+				</DragDropContext>
+			</div>
+		</Layout>
+	)
+}
