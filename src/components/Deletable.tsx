b5de2b14276679749aedce29ccf50f40b24286ac
Brian Clifford <bcliff323@gmail.com>
2022-10-04 23:03:48 -0400

    Move delete icon to component

--- src/components/Deletable.tsx
+++ src/components/Deletable.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Dialog } from "@reach/dialog";
-import { VisuallyHidden } from "@reach/visually-hidden";
+import DeleteIcon from "./DeleteIcon";
 import "@reach/dialog/styles.css";
 import "../css/styles.css";
 
@@ -31,17 +31,21 @@ export default function Deletable(props: Props) {
 				{children}
 			</div>
 			<button onClick={open}>
-				<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-					<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-				</svg>
+				<DeleteIcon />
 			</button>
 			<Dialog className="my-4 bg-cyan-50 rounded"
 				isOpen={showDialog}
 				onDismiss={close}>
 				<p>Are you sure you want to delete this?</p>
 				<div className="flex justify-end">
-					<button className="mt-3 mr-2 py-0.5 px-2.5 rounded bg-indigo-800 hover:bg-indigo-500 text-white" onClick={onDelete}>{confirmMessage}</button>
-					<button className="mt-3 py-0.5 px-2.5 rounded bg-indigo-200 hover:bg-indigo-100 text-indigo-800" onClick={close}>{cancelMessage}</button>
+					<button className="mt-3 mr-2 py-0.5 px-2.5 rounded bg-indigo-800 hover:bg-indigo-500 text-white"
+						onClick={onDelete}>
+						{confirmMessage}
+					</button>
+					<button className="mt-3 py-0.5 px-2.5 rounded bg-indigo-200 hover:bg-indigo-100 text-indigo-800"
+						onClick={close}>
+						{cancelMessage}
+					</button>
 				</div>
 			</Dialog>
 		</div>
