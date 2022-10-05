79d7347dbf3ce40c148c450b23eda8f168c46eab
Brian Clifford <bcliff323@gmail.com>
2022-10-04 23:14:52 -0400

    Move clipboard icon to component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Layout from '../components/Layout';
 import InputForm from '../components/InputForm';
 import Deletable from '../components/Deletable';
+import ClipboardIcon from '../components/ClipboardIcon';
 import { LocalStorageContext } from '../context/LocalStorageContext';
 import { TodoList } from '../types';
 import "../css/styles.css";
@@ -39,9 +40,7 @@ export default function Home(props: Props) {
 									cancelMessage="Cancel"
 									handleDelete={deleteList}>
 									<div className="flex">
-										<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-											<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
-										</svg>
+										<ClipboardIcon />
 										<Link key={i}
 											className="block flex-auto"
 											to={`/list/${(list as TodoList).id}`}>
