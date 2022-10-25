03e7f3c5d78010adb4552871e0d74c578aa22321
Brian Clifford <bcliff323@gmail.com>
2022-10-25 11:45:48 -0400

    Add error message tests

---
+++ src/__tests__/components/ErrorMessage.spec.jsx
@@ -0,0 +1,98 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
+import "@testing-library/jest-dom/extend-expect";
+import { givenTodoLists } from "../../helpers";
+import ErrorMessage from "../../components/ErrorMessage";
+import LocalStorageProvider from "../../components/LocalStorageProvider";
+import { ErrorTypes } from "../../types";
+import { LIST_NOT_FOUND_MSG, PAGE_NOT_FOUND_MSG } from "../../constants";
+
+describe("<ErrorMessage />", () => {
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it("should display a 404 error message", () => {
+		render(
+			<MemoryRouter>
+				<Routes>
+					<Route path="/" element={
+						<ErrorMessage errorType={ErrorTypes.PageNotFound} />
+					} />
+				</Routes>
+			</MemoryRouter>
+		);
+
+		const message = screen.getByTestId("error-message");
+		expect(message.textContent).toEqual(PAGE_NOT_FOUND_MSG);
+	});
+
+	it("should display a missing list error message", () => {
+		render(
+			<MemoryRouter>
+				<Routes>
+					<Route path="/" element={
+						<ErrorMessage errorType={ErrorTypes.ListNotFound} />
+					} />
+				</Routes>
+			</MemoryRouter>
+		);
+
+		const message = screen.getByTestId("error-message");
+		expect(message.textContent).toEqual(LIST_NOT_FOUND_MSG);
+	});
+
+	it('should display a link that directs to the homepage', () => {
+		render(
+			<MemoryRouter>
+				<Routes>
+					<Route path="/" element={
+						<ErrorMessage errorType={ErrorTypes.ListNotFound} />
+					} />
+				</Routes>
+			</MemoryRouter>
+		);
+
+		const homeLink = screen.getByTestId("error-button");
+		expect(homeLink).toHaveAttribute('href', '/');
+	});
+
+	it("should display list suggestion links if lists are saved", () => {
+		const lists = givenTodoLists(["List One", "List Two"]);
+		global.Storage.prototype.getItem = jest.fn(() => JSON.stringify(lists));
+
+		render(
+			<MemoryRouter initialEntries={[`/error`]}>
+				<Routes>
+					<Route path="/error" element={
+						<LocalStorageProvider>
+							<ErrorMessage errorType={ErrorTypes.ListNotFound} />
+						</LocalStorageProvider>
+					} />
+				</Routes>
+			</MemoryRouter>
+		);
+
+		const links = screen.getByTestId("error-view-lists");
+		expect(links.children.length).toEqual(2);
+	});
+
+	it("should omit list suggestion links if no lists are saved", () => {
+		global.Storage.prototype.getItem = jest.fn(() => JSON.stringify([]));
+
+		render(
+			<MemoryRouter initialEntries={[`/error`]}>
+				<Routes>
+					<Route path="/error" element={
+						<LocalStorageProvider>
+							<ErrorMessage errorType={ErrorTypes.ListNotFound} />
+						</LocalStorageProvider>
+					} />
+				</Routes>
+			</MemoryRouter>
+		);
+
+		const links = screen.queryByTestId("error-view-lists");
+		expect(links).not.toBeInTheDocument();
+	});
+});
