53b907f4453eb184c836f590f35c0e69b39e6181
Brian Clifford <bcliff323@gmail.com>
2022-10-27 21:03:01 -0400

    Focus confirm delete button when modal opens

--- src/components/Deletable.tsx
+++ src/components/Deletable.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Dialog } from "@reach/dialog";
 import DeleteIcon from "./icons/DeleteIcon";
 import "@reach/dialog/styles.css";
@@ -25,6 +25,7 @@ export default function Deletable(props: Props) {
 	const close = () => setShowDialog(false);
 	const open = () => setShowDialog(true);
 	const onDelete = () => handleDelete(id);
+	const buttonRef = useRef<HTMLButtonElement | null>(null);
 
 	return (
 		<div className="flex">
@@ -39,10 +40,12 @@ export default function Deletable(props: Props) {
 			<Dialog className="my-4 bg-cyan-50 rounded"
 				isOpen={showDialog}
 				onDismiss={close}
-				aria-label="Warning about permanently deleting item">
+				aria-label="Warning about permanently deleting item"
+				initialFocusRef={buttonRef}>
 				<p>Are you sure you want to delete this?</p>
 				<div className="flex justify-end">
 					<button data-testid="confirm-delete"
+						ref={buttonRef}
 						className="mt-3 mr-2 py-0.5 px-2.5 rounded bg-indigo-800 hover:bg-indigo-500 text-white"
 						onClick={onDelete}>
 						{confirmMessage}
