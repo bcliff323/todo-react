7e939eb6069d968641c31e42e09f0a8719759ddb
Brian Clifford <bcliff323@gmail.com>
2022-11-01 19:55:58 -0400

    Add a11y test cases for error message component

--- src/__tests__/components/ErrorMessage.spec.tsx
+++ src/__tests__/components/ErrorMessage.spec.tsx
@@ -1,11 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { axe, toHaveNoViolations } from "jest-axe";
 import "@testing-library/jest-dom/extend-expect";
 import { givenTodoLists } from "../../helpers";
 import ErrorMessage from "../../components/ErrorMessage";
 import LocalStorageProvider from "../../components/LocalStorageProvider";
 import { ErrorTypes } from "../../types";
 import { LIST_NOT_FOUND_MSG, PAGE_NOT_FOUND_MSG } from "../../constants";
+
+expect.extend(toHaveNoViolations);
 
 describe("<ErrorMessage />", () => {
 	afterEach(() => {
@@ -25,6 +28,20 @@ describe("<ErrorMessage />", () => {
 
 		const message = screen.getByTestId("error-message");
 		expect(message.textContent).toEqual(PAGE_NOT_FOUND_MSG);
+	});
+
+	it("should not violate accessibility rules when showing a 404 message", async () => {
+		const { container } = render(
+			<MemoryRouter>
+				<Routes>
+					<Route path="/" element={
+						<ErrorMessage errorType={ErrorTypes.PageNotFound} />
+					} />
+				</Routes>
+			</MemoryRouter>
+		);
+		const results = await axe(container);
+		expect(results).toHaveNoViolations();
 	});
 
 	it("should display a missing list error message", () => {
@@ -94,5 +111,24 @@ describe("<ErrorMessage />", () => {
 
 		const links = screen.queryByTestId("error-view-lists");
 		expect(links).not.toBeInTheDocument();
+	});
+
+	it("should not violate accessibility rules when displaying a missing list message", async () => {
+		const lists = givenTodoLists(["List One", "List Two"]);
+		global.Storage.prototype.getItem = jest.fn(() => JSON.stringify(lists));
+
+		const { container } = render(
+			<MemoryRouter initialEntries={[`/error`]}>
+				<Routes>
+					<Route path="/error" element={
+						<LocalStorageProvider>
+							<ErrorMessage errorType={ErrorTypes.ListNotFound} />
+						</LocalStorageProvider>
+					} />
+				</Routes>
+			</MemoryRouter>
+		);
+		const results = await axe(container);
+		expect(results).toHaveNoViolations();
 	});
 });
