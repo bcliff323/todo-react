383883772fe623ae63fd686bce351b5f745c73b3
Brian Clifford <bcliff323@gmail.com>
2022-11-01 20:09:07 -0400

    Add a11y test cases for message card component

--- src/__tests__/components/MessageCard.spec.tsx
+++ src/__tests__/components/MessageCard.spec.tsx
@@ -1,29 +1,43 @@
 import { render, screen } from "@testing-library/react";
+import { axe, toHaveNoViolations } from "jest-axe";
 import "@testing-library/jest-dom/extend-expect";
 import MessageCard from "../../components/MessageCard";
+
+expect.extend(toHaveNoViolations);
 
 describe("<Deletable />", () => {
 	const message = "This is a message";
 	const childText = "This is a child";
 
-	beforeEach(() => {
+	it("should render children", () => {
 		render(
 			<MessageCard message={message}>
 				<div>{childText}</div>
 			</MessageCard>
-		)
-	})
-	afterEach(() => {
-		jest.clearAllMocks();
-	});
+		);
 
-	it("should render children", () => {
 		const child = screen.getByText(childText);
 		expect(child).toBeInTheDocument();
 	});
 
 	it("should render an error message", () => {
+		render(
+			<MessageCard message={message}>
+				<div>{childText}</div>
+			</MessageCard>
+		);
+
 		const msg = screen.getByText(message);
 		expect(msg).toBeInTheDocument();
 	});
+
+	it("should not violate accessibility rules", async () => {
+		const { container } = render(
+			<MessageCard message={message}>
+				<div>{childText}</div>
+			</MessageCard>
+		);
+		const results = await axe(container);
+		expect(results).toHaveNoViolations();
+	})
 });
